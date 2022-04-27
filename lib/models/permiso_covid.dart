93e1180fee74cdbacd114591db5270ddd2f14bdc
Rafael Morales <rafaelmoralesvenegas@gmail.com>
2022-04-26 23:13:37 -0400

    Refactor segun sugerencias 2

--- lib/models/permiso_covid.dart
+++ lib/models/permiso_covid.dart
@@ -1,32 +1,46 @@
+import 'package:mi_utem/models/rut.dart';
+import 'package:mi_utem/models/usuario.dart';
+
 class PermisoCovid {
   String? id;
+  String? nombre;
+  String? rut;
+  String? codigoQr;
   String? perfil;
   String? motivo;
   String? campus;
   String? dependencia;
   String? jornada;
-  String? fechaSolicitud;
+  String? vigencia;
+  DateTime? fechaSolicitud;
 
   PermisoCovid(
     this.id,
+    this.nombre,
+    this.rut,
+    this.codigoQr,
     this.perfil,
     this.motivo,
     this.campus,
     this.dependencia,
     this.jornada,
+    this.vigencia,
     this.fechaSolicitud,
   );
 
   factory PermisoCovid.fromJson(Map<String, dynamic> json) {
     return PermisoCovid(
-      json['id'],
-      json['perfil'],
-      json['motivo'],
-      json['campus'],
-      json['dependencia'],
-      json['jornada'],
-      json['fechaSolicitud'],
-    );
+        json['id'],
+        json['usuario']?['nombreCompleto'],
+        json['usuario']?['rut'],
+        json['codigoQr'],
+        json['perfil'],
+        json['motivo'],
+        json['campus'],
+        json['dependencia'],
+        json['jornada'],
+        json['vigencia'],
+        DateTime.tryParse(json['fechaSolicitud']));
   }
 
   static List<PermisoCovid> fromJsonList(List<dynamic> json) {
